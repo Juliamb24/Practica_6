fc0e11111ac88da28483a4f709ebdae65b5d68b5
Julia_mb <juliasav2023@gmail.com>
2024-03-11 14:35:01 +0100

    Pintar users(ERROR)

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
+  usersServices = inject(UsersService)
+  arrUsers  = [];
+
+ // async ngOnInit(): Promise<void>{
+ //   this.arrUsers = await this.usersServices.getAllPromises()
+ // }
+  ngOnInit(): void {
+    this.usersServices.getAll().subscribe((data:IUser[]) => {
+      this.arrUsers = data;
+      console.log(this.arrUsers)
+    })
+  }
+
+
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { IUser } from '../interfaces/iuser.interface';
-import { lastValueFrom } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,10 @@ export class UsersService {
   httpClient = inject(HttpClient)
   baseUrl = 'https://peticiones.online/api/users' 
   
+
+  getAll(): Observable<IUser[]> {
+    return this.httpClient.get<IUser[]>(this.baseUrl)
+  }
 
   getAllPromises(): Promise<IUser[]>{
     return lastValueFrom(this.httpClient.get<IUser[]>(this.baseUrl))
