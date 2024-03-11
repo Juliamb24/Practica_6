d05c17ec6b04ea7ecaed4ad2842b4a2205470e7b
Julia_mb <juliasav2023@gmail.com>
2024-03-11 13:22:45 +0100

    Cargar usuarios + rutas

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { IUser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  httpClient = inject(HttpClient)
+  baseUrl = 'https://peticiones.online/api/users' 
+  
+
+  getAllPromises(): Promise<IUser[]>{
+    return lastValueFrom(this.httpClient.get<IUser[]>(this.baseUrl))
+  }
 }
