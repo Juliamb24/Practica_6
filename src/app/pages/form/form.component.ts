73f5922296c6c82fc6dc748ae8ec6cf080d7dd57
Julia_mb <juliasav2023@gmail.com>
2024-03-15 14:24:41 +0100

    Validadores form

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -15,16 +15,36 @@ export class FormComponent {
 
   constructor(){
     this.usersForm = new FormGroup({
-      first_name: new FormControl('',[]),
-      last_name: new FormControl('',[]),
-      username: new FormControl('',[]),
+      first_name: new FormControl('',[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      last_name: new FormControl('',[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      username: new FormControl('',[
+        Validators.required,
+      ]),
       image: new FormControl('',[]),
-      email: new FormControl('',[]),
-      password: new FormControl('',[]),
+      email: new FormControl('',[
+        Validators.required,
+      ]),
+      password: new FormControl('',[
+        Validators.required,
+      ]),
+      repassword: new FormControl('',[
+        Validators.required,
+      ]),
     },[])
   }
 
   getDataForm(){
-    console.log(this.usersForm.value)
+    console.log(this.usersForm.value),
+    this.usersForm.reset()
+  }
+
+  checkControl(formControlName: string, validador: string): boolean | undefined {
+    return this.usersForm.get(formControlName)?.hasError(validador) && this.usersForm.get(formControlName)?.touched
   }
 }
